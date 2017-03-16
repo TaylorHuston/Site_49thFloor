59111c80416984c6bc4f77e7ea0f3ff03ba24445
Taylor Huston <taylor.a.huston@gmail.com>
2017-03-15 20:48:22 -0700

    Add public directory to git for use with cloud cannon

---
+++ public/assets/js/mobileNav.js
@@ -0,0 +1,20 @@
+$('a[href^="#"]').on('click', function (event) {
+
+  var target = $($(this).attr('href'));
+
+  if (target.length) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 1000);
+  }
+});
+
+$('#hamburger').click(function () {
+  $('nav').toggleClass('expand');
+});
+
+$('nav').click(function () {
+  $('nav').toggleClass('expand');
+});
